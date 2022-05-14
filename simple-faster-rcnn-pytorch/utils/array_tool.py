a661705819909ce523e7bc40e5956ae22c918b60
HugoGranstrom <5092565+HugoGranstrom@users.noreply.github.com>
2022-05-14 15:10:42 +0200

    try faster to work on gpu

--- simple-faster-rcnn-pytorch/utils/array_tool.py
+++ simple-faster-rcnn-pytorch/utils/array_tool.py
@@ -4,7 +4,6 @@ tools to convert specified type
 import torch as t
 import numpy as np
 
-
 def tonumpy(data):
     if isinstance(data, np.ndarray):
         return data
@@ -12,13 +11,13 @@ def tonumpy(data):
         return data.detach().cpu().numpy()
 
 
-def totensor(data, cuda=False):
+def totensor(data, cuda=True):
     if isinstance(data, np.ndarray):
         tensor = t.from_numpy(data)
     if isinstance(data, t.Tensor):
         tensor = data.detach()
-    # if cuda:
-    #     tensor = tensor.cuda()
+    if cuda and torch.cuda.is_available():
+        tensor = tensor.cuda()
     return tensor
 
 
